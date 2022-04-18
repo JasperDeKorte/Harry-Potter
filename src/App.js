b6cc7b60f7bcc3f02119a89ff4794bdcf9731f58
JasperDeKorte <jasperDKcode@gmail.com>
2022-04-18 19:49:24 +0200

    added api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [characters, setCharacters] = useState();
+  console.log({ characters });
+  useEffect(() => {
+    async function fetchData() {
+      await axios
+        .get("http://hp-api.herokuapp.com/api/characters")
+        .then((response) => {
+          setCharacters(response.data);
+        });
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {characters &&
+        characters.map((result) => {
+          return (
+            <div>
+              <div>
+                {result.name} {result.house && "- " + result.house}
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
